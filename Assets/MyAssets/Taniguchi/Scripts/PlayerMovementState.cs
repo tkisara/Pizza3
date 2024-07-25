6d14aab1e06133ea83c1203153168d37f0b8a126
nattupurin <gamecreatemulch@gmail.com>
2024-07-25 14:36:22 +0900

    state

--- Assets/MyAssets/Taniguchi/Scripts/PlayerMovementState.cs
+++ Assets/MyAssets/Taniguchi/Scripts/PlayerMovementState.cs
@@ -1,46 +1,44 @@
 using UnityEngine;
 
-public class PlayerIdleState:PlayerState
+public class PlayerMovementState : PlayerState
 {
-    public PlayerIdleState(PlayerStateContext context,PlayerStateMachine.PlayerStates estate):base(context, estate)
+    public PlayerMovementState(PlayerStateContext context, PlayerStateMachine.PlayerStates estate) : base(context, estate)
     {
         PlayerStateContext Context = context;
     }
-
-    
     public override void EnterState()
     {
-        Debug.Log("IdleState開始");
+        Debug.Log("MovementState開始");
     }
-    
+
     public override void ExitState()
     {
-        Debug.Log("IdleState終了");
+        Debug.Log("MovementState終了");
     }
-    
+
     public override void UpdateState()
     {
-        Debug.Log("IdleState中");
+        Debug.Log("MovementState中");
     }
     public override PlayerStateMachine.PlayerStates GetNextState()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            return PlayerStateMachine.PlayerStates.Movement;
+            return PlayerStateMachine.PlayerStates.Dead;
         }
         return StateKey;
     }
-    
+
     public override void OnTriggerEnter(Collider other)
     {
 
     }
-    
+
     public override void OnTriggerStay(Collider other)
     {
 
     }
-    
+
     public override void OnTriggerExit(Collider other)
     {
 
