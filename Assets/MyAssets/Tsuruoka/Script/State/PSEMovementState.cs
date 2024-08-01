b559ea25f4dffeef457c8d160b2382acdd347111
23ci0124 <23ci0124@jec.ac.jp>
2024-08-01 19:48:23 +0900

    a

---
+++ Assets/MyAssets/Tsuruoka/Script/State/PSEMovementState.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using static PSEStateMachine;
+
+public class PSEMovementState : PSEState
+{
+    //----------------------------------------------------------------------------------------------
+    public PSEMovementState(PSEStateContext context, PSEStateMachine.PSEStates estate) : base(context, estate)
+    {
+        PSEStateContext Context = context;
+    }
+    //----------------------------------------------------------------------------------------------
+    //開始時に呼び出される関数
+    public override void EnterState()
+    {
+        Debug.Log("WalkState開始");
+    }
+    //----------------------------------------------------------------------------------------------
+    //Stateを抜けるときに呼び出される関数
+    public override void ExitState()
+    {
+        Debug.Log("WalkState終了");
+    }
+    //----------------------------------------------------------------------------------------------
+    // 呼び出されている間処理を行う関数
+    public override void UpdateState()
+    {
+        Debug.Log("WalkState中");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+
+    }
+    //----------------------------------------------------------------------------------------------
+    //
+    public override PSEStateMachine.PSEStates GetNextState()
+    {
+        return StateKey;
+    }
+    //----------------------------------------------------------------------------------------------
+    //当たり判定(触れたら)
+    public override void OnTriggerEnter(Collider other)
+    {
+
+    }
+    //----------------------------------------------------------------------------------------------
+    //当たり判定(触れてる間)
+    public override void OnTriggerStay(Collider other)
+    {
+
+    }
+    //----------------------------------------------------------------------------------------------
+    //当たり判定(抜けたら)
+    public override void OnTriggerExit(Collider other)
+    {
+
+    }
+}
