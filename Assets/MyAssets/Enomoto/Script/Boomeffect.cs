9418082dae46cdd965a7810156634c701616fad5
23ci0107 <135036412+0107-Eno@users.noreply.github.com>
2024-10-23 12:30:14 +0900

    エフェクト

--- Assets/MyAssets/Enomoto/Script/Boomeffect.cs
+++ Assets/MyAssets/Enomoto/Script/Boomeffect.cs
@@ -4,17 +4,30 @@ using static UnityEngine.ParticleSystem;
 public class TriggerEffect : MonoBehaviour
 {
     // エフェクトのプレハブ（事前に作成したパーティクルシステムなどをアサイン）
-   [SerializeField] private ParticleSystem effectBoom;
-   [SerializeField] private ParticleSystem effectEx;
+  
+   [SerializeField]
+   [Tooltip("発生させるエフェクト(パーティクル)")]
+   private ParticleSystem effectBoom;
+   [SerializeField]
+   [Tooltip("発生させるエフェクト(パーティクル)")]
+   private ParticleSystem effectEx;
 
     // トリガーに入ったときに呼ばれるメソッド
-    private void OnTriggerEnter(Collider other)
+    /// <summary>
+	/// 衝突した時
+	/// </summary>
+	/// <param name="collision"></param>
+    private void OnCollisionEnter(Collision other)
     {
         // タグが "Player" の場合のみエフェクトを発生させる
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("当たった");
-            // パーティクルシステムのインスタンスを生成する。
+
+            effectBoom.Play();
+            effectEx.Play();
+
+            /*// パーティクルシステムのインスタンスを生成する。
             ParticleSystem newParticle0 = Instantiate(effectBoom);
             ParticleSystem newParticle1 = Instantiate(effectEx);
             // パーティクルの発生場所をこのスクリプトをアタッチしているGameObjectの場所にする。
@@ -26,7 +39,7 @@ public class TriggerEffect : MonoBehaviour
             // インスタンス化したパーティクルシステムのGameObjectを5秒後に削除する。(任意)
             // ※第一引数をnewParticleだけにするとコンポーネントしか削除されない。
             Destroy(newParticle0.gameObject, 5.0f);
-            Destroy(newParticle1.gameObject, 5.0f);
+            Destroy(newParticle1.gameObject, 5.0f);*/
         }
     }
 }
